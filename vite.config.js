131a0f83c55ba3d53962228f43717ffc15bd122c
SHERLOCKx90 <sherlockx90@gmail.com>
2024-08-25 13:51:26 +0530

    proxy fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://52.224.54.204:8010',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+})
